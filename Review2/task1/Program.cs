6b5bed1c4f0aaeb3f304634aed9a2f618850a9bd
komalupadhyay02 <komal.upadhyay_cs.da21@gla.ac.in>
2025-02-05 15:28:02 +0530

    [KOMAL] ADD:REVIEW2

---
+++ Review2/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Character
+{
+    static void CountCharacters(string input)
+    {
+        int[] charCount = new int[256]; 
+
+       
+        foreach (char ch in input)
+        {
+            charCount[ch]++;
+        }
+
+        
+        Console.WriteLine("Character Occurence:");
+        for (int i = 0; i < 256; i++)
+        {
+            if (charCount[i] > 0)
+            {
+                Console.WriteLine($"'{(char)i}' : {charCount[i]}");
+            }
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        CountCharacters(input);
+    }
+}
